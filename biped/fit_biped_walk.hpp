b45efd264d12c8aea248219fec477d47a4cf3e1f
Léni K. Le Goff <lenilegoff@gmail.com>
2019-08-27 12:45:55 +0200

    parallel working version

--- biped/fit_biped_walk.hpp
+++ biped/fit_biped_walk.hpp
@@ -12,7 +12,7 @@ namespace sf = sferes;
 SFERES_FITNESS(FitBipedWalk,sf::fit::Fitness){
     public:
     FitBipedWalk(){
-        _simu.reset(new biped::Simulation(biped::Params::biped::actuator_type,biped::Params::simu::model_path()));
+        // _simu.reset(new biped::Simulation(biped::Params::biped::actuator_type,biped::Params::simu::model_path()));
     }
 
     template<typename Indiv>
@@ -23,11 +23,12 @@ SFERES_FITNESS(FitBipedWalk,sf::fit::Fitness){
 
         this->_objs.resize(1);
 
-        _simu->reset();
+        biped::Simulation simu(biped::Params::biped::actuator_type,biped::Params::simu::model_path());
+        // _simu.reset();
 
 #ifdef VISU
         std::cout << "Initiatlization of the visualisation" << std::endl;
-        _simu->init_visu();
+        simu.init_visu();
 #endif
 
 
@@ -35,26 +36,26 @@ SFERES_FITNESS(FitBipedWalk,sf::fit::Fitness){
 
 //            std::cout << "eval ... step " << i << std::endl;
 
-            get_inputs();
+            get_inputs(simu);
 
             if(pos_bd.size() < biped::Params::novelty::nb_pos
                     && i > 0
                     && (i%(int)std::round(biped::Params::simu::nb_steps/biped::Params::novelty::nb_pos)==0)){
-                        if(!std::isnan(_simu->_controller->get_biped()->getBodyNode("h_pelvis")->getWorldTransform().matrix().col(3)(0)))
-                            pos_bd.push_back(_simu->_controller->get_biped()->getBodyNode("h_pelvis")->getWorldTransform().matrix().col(3));
+                        if(!std::isnan(simu._controller->get_biped()->getBodyNode("h_pelvis")->getWorldTransform().matrix().col(3)(0)))
+                            pos_bd.push_back(simu._controller->get_biped()->getBodyNode("h_pelvis")->getWorldTransform().matrix().col(3));
                         else pos_bd.push_back(Eigen::VectorXd::Zero(3));
                     }
 
-            step_check(ind.nn());
+            step_check(ind.nn(),simu);
 
-            move_check();
+            move_check(simu);
 
-            _simu->update(i);
+            simu.update(i);
 
             i++;
         }
-        if(!std::isnan(_simu->_controller->get_biped()->getBodyNode("h_pelvis")->getWorldTransform().matrix().col(3)(0)))
-            pos_bd.push_back(_simu->_controller->get_biped()->getBodyNode("h_pelvis")->getWorldTransform().matrix().col(3));
+        if(!std::isnan(simu._controller->get_biped()->getBodyNode("h_pelvis")->getWorldTransform().matrix().col(3)(0)))
+            pos_bd.push_back(simu._controller->get_biped()->getBodyNode("h_pelvis")->getWorldTransform().matrix().col(3));
         else pos_bd.push_back(Eigen::VectorXd::Zero(3));
         std::cout << "POS BD :" << std::endl;
         for(auto pos : pos_bd)
@@ -79,16 +80,16 @@ SFERES_FITNESS(FitBipedWalk,sf::fit::Fitness){
 
     //TODO functions to convert [lower,upper] <-> [0,1]
 
-    void get_inputs(){
+    void get_inputs(biped::Simulation& simu){
         Eigen::VectorXd feedback;
         if(biped::Params::biped::fb_type == biped::feedback_type::POSITION)
-            feedback = _simu->_controller->get_biped()->getPositions();
+            feedback = simu._controller->get_biped()->getPositions();
         else if(biped::Params::biped::fb_type == biped::feedback_type::VELOCITIES)
-            feedback = _simu->_controller->get_biped()->getVelocities();
+            feedback = simu._controller->get_biped()->getVelocities();
         else if(biped::Params::biped::fb_type == biped::feedback_type::ACCELERATION)
-            feedback = _simu->_controller->get_biped()->getAccelerations();
+            feedback = simu._controller->get_biped()->getAccelerations();
         else if(biped::Params::biped::fb_type == biped::feedback_type::FORCES)
-            feedback = _simu->_controller->get_biped()->getForces();
+            feedback = simu._controller->get_biped()->getForces();
 
         inputs.resize(feedback.rows());
         Eigen::VectorXd lower_limits;
@@ -96,8 +97,8 @@ SFERES_FITNESS(FitBipedWalk,sf::fit::Fitness){
         double lower, upper;
         int k = 0;
         for(int i = 0; i < inputs.size();){
-            lower_limits = _simu->_controller->get_biped()->getJoint(k)->getPositionLowerLimits();
-            upper_limits = _simu->_controller->get_biped()->getJoint(k)->getPositionUpperLimits();
+            lower_limits = simu._controller->get_biped()->getJoint(k)->getPositionLowerLimits();
+            upper_limits = simu._controller->get_biped()->getJoint(k)->getPositionUpperLimits();
             for(int j = 0; j < lower_limits.rows(); j++){
                 lower = lower_limits(j);
                 upper = upper_limits(j);
@@ -117,7 +118,7 @@ SFERES_FITNESS(FitBipedWalk,sf::fit::Fitness){
     }
 
     template<typename NN>
-    void step_check(NN &nn){
+    void step_check(NN &nn, biped::Simulation& simu){
         nn.step(inputs);
         int index;
         Eigen::VectorXd lower_limits;
@@ -126,8 +127,8 @@ SFERES_FITNESS(FitBipedWalk,sf::fit::Fitness){
         outf.resize(nn.get_outf().size());
         int i = 0;
         for(const std::string &jname : biped::Controller::joint_names){
-            lower_limits = _simu->_controller->get_biped()->getJoint(jname)->getVelocityLowerLimits();
-            upper_limits = _simu->_controller->get_biped()->getJoint(jname)->getVelocityUpperLimits();
+            lower_limits = simu._controller->get_biped()->getJoint(jname)->getVelocityLowerLimits();
+            upper_limits = simu._controller->get_biped()->getJoint(jname)->getVelocityUpperLimits();
             for(int j = 0; j < lower_limits.rows(); j++){
                 lower = lower_limits(j);
                 upper = upper_limits(j);
@@ -144,9 +145,9 @@ SFERES_FITNESS(FitBipedWalk,sf::fit::Fitness){
 //        std::cout << std::endl;
     }
 
-    void move_check(){
+    void move_check(biped::Simulation& simu){
         for(int i = 0; i < outf.size(); i++){
-            _simu->_controller->setCommands(biped::Controller::dof_names[i],outf[i]);
+            simu._controller->setCommands(biped::Controller::dof_names[i],outf[i]);
         }
     }
 
@@ -154,7 +155,7 @@ SFERES_FITNESS(FitBipedWalk,sf::fit::Fitness){
 
     bool stop_eval = false;
 
-    std::shared_ptr<biped::Simulation> _simu;
+    // std::shared_ptr<biped::Simulation> _simu;
     std::vector<float> outf, inputs;
 };
 
